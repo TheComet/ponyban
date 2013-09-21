3022ebaa005076b38fbf6b792203e4561146b145
thecomet <alex.murray@gmx.ch>
2013-09-21 20:20:30 +0200

    updated license info at the top of each source file

--- include/App.hpp
+++ include/App.hpp
@@ -1,18 +1,18 @@
 /*
- * This file is part of Chocobun.
+ * This file is part of Ponyban.
  *
- * Chocobun is free software: you can redistribute it and/or modify
+ * Ponyban is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * Chocobun is distributed in the hope that it will be useful,
+ * Ponyban is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with Chocobun.  If not, see <http://www.gnu.org/licenses/>.
+ * along with Ponyban.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 // ----------------------------------------------------------------------------
