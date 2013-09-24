c9f6637fc35ba59cc7b5dc40bf278468441d9757
thecomet <alex.murray@gmx.ch>
2013-09-24 22:16:50 +0200

    Improved documentation for animated sprite class

--- include/AnimatedSprite.hpp
+++ include/AnimatedSprite.hpp
@@ -31,6 +31,36 @@
 // ----------------------------------------------------------------------------
 // forward declarations
 
+/*!
+ * @brief Handles animated sprites
+ * Animated sprites are usually saved as one image containing a grid
+ * of frames. This class handles loading, splitting, and playing them.
+ * You are able to customize how many frames are contained within an image,
+ * and you have control over playing only parts of an animation.
+ *
+ * Example code:
+ * @code
+ * // load the sprite
+ * AnimatedSprite mySprite;
+ * if( !mySprite.loadFromFile("test.gif", 8, 8 ) // the sprite sheet contains 8x8 frames (64)
+ *     return;
+ *
+ * // our idle frame is from frame 2-15
+ * mySprite.play(2,15);
+ *
+ * // in your main loop...
+ * sf::Clock clock;
+ * while(1)
+ * {
+ *
+ *     // calculate elapsed time, which is used to update all animated sprites
+ *     sf::Time elapsedTime = clock.restart();
+ *
+ *     // update sprites here (if you have more, some kind of for loop)
+ *     mySprite.updateFrame( elapsedTime );
+ * }
+ * @endcode
+ */
 class AnimatedSprite
 {
 public:
@@ -47,7 +77,7 @@ public:
 
     /*! @brief Loads an image file and creates an animated sprite from it
      *
-     * Animated sprites are usually saved as a sheet of frames arranged in a grid.
+     * Animated sprites are usually saved as a single image of frames arranged in a grid.
      * This class handles loading them, splitting them, and playing the animation.
      * Sometimes, animations don't have enough frames to fill a sprite sheet symmetrically,
      * in which case the number of frames can optionally be provided. If not, it is assumed there
@@ -64,37 +94,62 @@ public:
 
     /*!
      * @brief Sets the position of the sprite
+     * The position is relative to the top-left corner of the sprite,
+     * and sets the position in pixels on the screen.
+     * @param x The x-coordinate to set it to
+     * @param y The y-coordinate to set it to
      */
     void setPosition( const float& x, const float& y );
 
     /*!
      * @brief Sets the global scale of the sprite
+     * The scale is a factor of the original size, which by
+     * default is 1,1
+     * @param x The x-scale
+     * @param y The y-scale
      */
     void setScale( const float& x, const float& y );
 
     /*!
      * @brief Starts playing the animation
+     * The animation will begin to play with the specified
+     * time delay between frames (see @a setFrameDelay). When the animation
+     * reaches the end of its frames, it will loop around and begin playing
+     * from the beginning again. If there is only one frame, nothing will
+     * happen.
      */
     void play( void );
 
     /*!
      * @brief Starts playing the animation between startFrame and endFrame
+     * The animation will begin to play with the specified time delay between
+     * frames (see @a setFrameDelay) from the beginning frame to the end frame.
+     * When the animation reaches the end of its frames, it will loop around and
+     * begin playing from the beginning again. If there is only one frame, nothing
+     * will happend.
+     * @param startFrame The frame to begin playing from
+     * @param endFrame The frame to stop at and wrap around again
      */
     void play( const unsigned long& startFrame, const unsigned long& endFrame );
 
     /*!
      * @brief Stops playing the animation
+     * Will halt the animation and display the current frame, until the animation
+     * is resumed again.
      */
     void stop( void );
 
     /*!
      * @brief Sets the current frame of the animatino
+     * If the specified frame is larger than the total frames of the animation,
+     * this method will silently fail.
+     * @param frame The frame to set
      */
     void setFrame( const unsigned long& frame );
 
     /*!
      * @brief Sets the time between animation frames
-     * Default is 15ms
+     * Default is 15ms. Sets the time between frames when playing.
      * @param time The time to set it to
      */
     void setFrameDelay( const sf::Time& time );
@@ -108,7 +163,7 @@ public:
     void updateFrame( const sf::Time& timePassed );
 
     /*!
-     * @brief Gets the underlying sprite
+     * @brief Gets the underlying sprite object. This object is read-only.
      */
     const sf::Sprite& getSprite( void ) const;
 
